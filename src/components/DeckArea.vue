fd9ca45ddf09011283400de161bf05fdc565f390
jessedagriffin <jesse.w.griffin@wsu.edu>
2024-01-06 14:50:44 -0800

    Refactored setup scripts. Updated cursor styling. Added logic for tracking last card. Added logic for tracking updating score for player who clicked first.

--- src/components/DeckArea.vue
+++ src/components/DeckArea.vue
@@ -9,89 +9,100 @@
     </div>
 </template>
 
-<script setup>
-    import { ref, onMounted, computed, watch } from 'vue';
+<script>
+import { ref, onMounted, computed, watch } from 'vue';
+    
+export default {
+    setup(props, ctx) {
+        const deck = ref([]);
 
-    const deck = ref([]);
+        const flipNextCard = () => {
+            const randomIndex = Math.floor(Math.random() * deck.value.length);
+            const randomCard = deck.value[randomIndex];
+            deck.value.splice(randomIndex, 1)
+            // console.log(randomCard);
 
-    const flipNextCard = () => {
-        const randomIndex = Math.floor(Math.random() * deck.value.length);
-        const randomCard = deck.value[randomIndex];
-        deck.value.splice(randomIndex, 1)
-        console.log(randomCard);
+            ctx.emit('set-last-card-flipped', randomCard);
 
-        const newCard = document.getElementById("remaining-cards").cloneNode();
-        newCard.src = '../src/assets/cardBack.png';
-        newCard.id = randomCard;
-        newCard.className = 'flipped-card';
-        
-        document.getElementById("deck-area").appendChild(newCard);
-        
-        
-        // Card flip speed
-        const speed = Math.floor(Math.random() * 300) + 50;
-        newCard.style["-webkit-animation-duration"] = `${speed / 500}s`;
-        console.log(speed);
-        newCard.classList.add('move-img');
-        setTimeout(() => {
-            newCard.src = `../src/assets/cardFaces/${randomCard}.png`;
-        }, speed);
+            const newCard = document.getElementById("remaining-cards").cloneNode();
+            newCard.src = '../src/assets/cardBack.png';
+            newCard.id = randomCard;
+            newCard.className = 'flipped-card';
+            
+            document.getElementById("deck-area").appendChild(newCard);
+            
+            
+            // Card flip speed
+            const speed = Math.floor(Math.random() * 300) + 50;
+            newCard.style["-webkit-animation-duration"] = `${speed / 500}s`;
+            // console.log(speed);
+            newCard.classList.add('move-img');
+            setTimeout(() => {
+                newCard.src = `../src/assets/cardFaces/${randomCard}.png`;
+            }, speed);
+        }
+
+        onMounted(() => {
+            deck.value = [
+                '2_of_clubs',
+                '2_of_diamonds',
+                '2_of_hearts',
+                '2_of_spades',
+                '3_of_clubs',
+                '3_of_diamonds',
+                '3_of_hearts',
+                '3_of_spades',
+                '4_of_clubs',
+                '4_of_diamonds',
+                '4_of_hearts',
+                '4_of_spades',
+                '5_of_clubs',
+                '5_of_diamonds',
+                '5_of_hearts',
+                '5_of_spades',
+                '6_of_clubs',
+                '6_of_diamonds',
+                '6_of_hearts',
+                '6_of_spades',
+                '7_of_clubs',
+                '7_of_diamonds',
+                '7_of_hearts',
+                '7_of_spades',
+                '8_of_clubs',
+                '8_of_diamonds',
+                '8_of_hearts',
+                '8_of_spades',
+                '9_of_clubs',
+                '9_of_diamonds',
+                '9_of_hearts',
+                '9_of_spades',
+                '10_of_clubs',
+                '10_of_diamonds',
+                '10_of_hearts',
+                '10_of_spades',
+                'ace_of_clubs',
+                'ace_of_diamonds',
+                'ace_of_hearts',
+                'ace_of_spades',
+                'jack_of_clubs2',
+                'jack_of_diamonds2',
+                'jack_of_hearts2',
+                'jack_of_spades2',
+                'king_of_clubs2',
+                'king_of_diamonds2',
+                'king_of_hearts2',
+                'king_of_spades2',
+                'queen_of_clubs2',
+                'queen_of_diamonds2',
+                'queen_of_hearts2',
+                'queen_of_spades2'
+            ];
+        });
+
+        return {
+            deck,
+            flipNextCard,
+        };
     }
-
-    onMounted(() => {
-        deck.value = [
-            '2_of_clubs',
-            '2_of_diamonds',
-            '2_of_hearts',
-            '2_of_spades',
-            '3_of_clubs',
-            '3_of_diamonds',
-            '3_of_hearts',
-            '3_of_spades',
-            '4_of_clubs',
-            '4_of_diamonds',
-            '4_of_hearts',
-            '4_of_spades',
-            '5_of_clubs',
-            '5_of_diamonds',
-            '5_of_hearts',
-            '5_of_spades',
-            '6_of_clubs',
-            '6_of_diamonds',
-            '6_of_hearts',
-            '6_of_spades',
-            '7_of_clubs',
-            '7_of_diamonds',
-            '7_of_hearts',
-            '7_of_spades',
-            '8_of_clubs',
-            '8_of_diamonds',
-            '8_of_hearts',
-            '8_of_spades',
-            '9_of_clubs',
-            '9_of_diamonds',
-            '9_of_hearts',
-            '9_of_spades',
-            '10_of_clubs',
-            '10_of_diamonds',
-            '10_of_hearts',
-            '10_of_spades',
-            'ace_of_clubs',
-            'ace_of_diamonds',
-            'ace_of_hearts',
-            'ace_of_spades',
-            'jack_of_clubs2',
-            'jack_of_diamonds2',
-            'jack_of_hearts2',
-            'jack_of_spades2',
-            'king_of_clubs2',
-            'king_of_diamonds2',
-            'king_of_hearts2',
-            'king_of_spades2',
-            'queen_of_clubs2',
-            'queen_of_diamonds2',
-            'queen_of_hearts2',
-            'queen_of_spades2'
-        ];
-    });
+}
 </script>
--- src/components/pages/InGame.vue
+++ src/components/pages/InGame.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="game-board">
+    <div id="game-board" >
         <div id="left-column">
             <score-board v-bind="score" />
         </div>
         <!-- <div class="break-v"></div> -->
         <div id="center-column">
-            <deck-area />
+            <deck-area @set-last-card-flipped="setLastCardFlipped" />
         </div>
         <div id="right-column">
             <!-- <score-board v-bind="score" /> -->
@@ -14,20 +14,78 @@
     <!-- <input type="button" @click="inc"/> -->
 </template>
 
-<script setup>
+<script>
 import ScoreBoard from '../ScoreBoard.vue';
 import DeckArea from '../DeckArea.vue';
 
 import { ref, onMounted, computed, watch } from 'vue';
 
-const score = ref({
-    player1: 1,
-    player2: 0
-});
+export default {
+    components: {
+        ScoreBoard,
+        DeckArea,
+    },
+    setup() {
+        const playerOneKey = 'ControlLeft';
+        const playerTwoKey = 'ControlRight';
 
-// function inc() {
-//     score.value.player1 = score.value.player1 + 1;
-//     console.log('inc');
-//     console.log(score);
-// }
+        const score = ref({
+            player1: 0,
+            player2: 0
+        });
+
+        const lastCardFlipped = ref('');
+
+        const lastPlayerClicked = ref('');
+
+        const setLastCardFlipped = (value) => {
+            console.log('setting last card flipped', value);
+            lastCardFlipped.value = value;
+        }
+
+        const handlePlayerInput = (value) => {
+            if(lastCardFlipped.value.includes('jack')) {
+                if(lastPlayerClicked.value === 'player1') {
+                    score.value.player1 = score.value.player1 + 1;
+                } else if(lastPlayerClicked.value === 'player2') {
+                    score.value.player2 = score.value.player2 + 1;
+                }
+                lastPlayerClicked.value = '';
+                lastCardFlipped.value = '';
+            } else {
+                lastPlayerClicked.value = '';
+                // alert('That was not a Jack!');
+            }
+        }
+
+        onMounted(() => {
+            window.addEventListener('keyup', (event) => {
+                // console.log(
+                //     event.code,
+                //     lastCardFlipped.value,
+                //     lastPlayerClicked.value,
+                // );
+
+                if (event.code === 'Space') {
+                    alert('How to Play... Will be added later');
+                } else if (event.code === playerOneKey) {
+                    lastPlayerClicked.value === '' ? lastPlayerClicked.value = 'player1' : '';
+                } else if (event.code === playerTwoKey) {
+                    lastPlayerClicked.value === '' ? lastPlayerClicked.value = 'player2' : '';
+                }
+
+                if(lastPlayerClicked.value !== '' && lastCardFlipped.value !== '') {
+                    handlePlayerInput();
+                }
+            })
+        });
+        
+        return {
+            score,
+            lastPlayerClicked,
+            setLastCardFlipped,
+        };
+    },
+}
+
 </script>
