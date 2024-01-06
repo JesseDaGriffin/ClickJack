ba26de1c42a106597aa30df3362e8599df27becc
jessedagriffin <jesse.w.griffin@wsu.edu>
2024-01-06 01:32:19 -0800

    Updated styling of scoreboard. Created site wide font. Add scaling to card flipping animation.

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,11 +1,17 @@
 <template>
     <div id="scoreboard">
-        <h2>Score</h2>
-        <div class="player-one">
+        <h2 id="scoreboard-title">Score</h2>
+        <!-- <div class="player-one">
             <p>Player 1: {{props.player1}}</p>
         </div>
         <div class="player-two">
             <p>Player 2: {{props.player2}}</p>
+        </div> -->
+        <div id="scoreboard-grid">
+            <div>Player 1</div>
+            <div>Player 2</div>
+            <div class="score-count">{{props.player1}}</div>
+            <div class="score-count">{{props.player2}}</div>
         </div>
     </div>
 </template>
